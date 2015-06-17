e7a9257394dac2154bb1f9fcc4c1d9b49f22ad88
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2015-06-17 01:36:02 -0300

    pypluto.pload.grid now working
    had to correct line numbers and issues with columns

--- pyPLUTO.py
+++ pyPLUTO.py
@@ -154,48 +154,48 @@ class pload(object):
 	dx2 - Array dx2\n
 	dx3 - Array dx3\n
 	"""
-	    
-        fname_g = self.wdir+"grid.out"
-        f_grid=open(fname_g)
-        lnum_g = len(f_grid.readlines())
-        # have to make smart code that does pattern matching to find values of n below
-        #####
-        n1 = linecache.getline(fname_g,10)
-        n2 = linecache.getline(fname_g,int(n1)+11)
-        n3 = linecache.getline(fname_g,int(n1)+int(n2)+12)
-        x1=[]
-        x2=[]
-        x3=[]
-        dx1=[]
-        dx2=[]
-        dx3=[]
-        for i in range(2,int(n1)+2):
-            A = linecache.getline(fname_g,i).split()
-            x1.append(float(A[2]))
-            dx1.append(float(A[4]))
-        
-        x1 = np.asarray(x1)
-        dx1 = np.asarray(dx1)
+	fname_g = self.wdir+"grid.out"
+	f_grid= open(fname_g)
+	lnum_g = len(f_grid.readlines())
+	# have to make smart code that does pattern matching to find values of n below
+	#####
+	n1 = linecache.getline(fname_g,10)
+	n2 = linecache.getline(fname_g,int(n1)+11)
+	n3 = linecache.getline(fname_g,int(n1)+int(n2)+12)
+	x1=[]
+	x2=[]
+	x3=[]
+	dx1=[]
+	dx2=[]
+	dx3=[]
 
-        for j in range(3+int(n1),int(n1)+int(n2)+3):
-            B = linecache.getline(fname_g,j).split()
-            x2.append(float(B[2]))
-            dx2.append(float(B[4]))
-        
-        x2 = np.asarray(x2)
-        dx2 = np.asarray(dx2)
+	for i in range(11,int(n1)+10):
+		A = linecache.getline(fname_g,i).split()
+		x1.append(float(A[1]))
+		dx1.append(float(A[2]))
 
-        for k in range(4+int(n1)+int(n2),lnum_g+1):
-            C = linecache.getline(fname_g,k).split()
-            x3.append(float(C[2]))
-            dx3.append(float(C[4]))
+	x1 = np.asarray(x1)
+	dx1 = np.asarray(dx1)
 
-        x3 = np.asarray(x3)
-        dx3 = np.asarray(dx3)
+	for j in range(12+int(n1),int(n1)+int(n2)+11):
+	    B = linecache.getline(fname_g,j).split()
+	    x2.append(float(B[1]))
+	    dx2.append(float(B[2]))
+	 
+	x2 = np.asarray(x2)
+	dx2 = np.asarray(dx2)
 
-        f_grid.close()
-	
-        grid_dict={'n1':int(n1),'n2':int(n2),'n3':int(n3),'x1':x1,'x2':x2,'x3':x3,'dx1':dx1,'dx2':dx2,'dx3':dx3}
+	for k in range(13+int(n1)+int(n2),lnum_g+1):
+	    C = linecache.getline(fname_g,k).split()
+	    x3.append(float(C[1]))
+	    dx3.append(float(C[2]))
+
+	x3 = np.asarray(x3)
+	dx3 = np.asarray(dx3)
+
+	f_grid.close()
+	 
+	grid_dict={'n1':int(n1),'n2':int(n2),'n3':int(n3),'x1':x1,'x2':x2,'x3':x3,'dx1':dx1,'dx2':dx2,'dx3':dx3}
 
 	return grid_dict
 
